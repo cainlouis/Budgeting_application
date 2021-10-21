199f4676dc69725b00f16222da3869917e571ac8
Zacharie Makeen <zach.makeen@gmail.com>
2021-10-21 13:31:44 -0400

    Add menu item lick handler.

--- app/src/main/java/com/example/budgetingapplication/MainActivity.kt
+++ app/src/main/java/com/example/budgetingapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,28 @@ class MainActivity : AppCompatActivity() {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.budgeting_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.about -> {
+//                newGame()
+                true
+            }
+            R.id.add_spending -> {
+//                showHelp()
+                true
+            }
+            R.id.clear_all_entries -> {
+//                showHelp()
+                true
+            }
+            R.id.display_current_status -> {
+//                showHelp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
