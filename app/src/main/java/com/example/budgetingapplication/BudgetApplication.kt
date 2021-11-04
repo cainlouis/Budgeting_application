54f81cef4cf051e2b42b00b9ad9ffd41192d6556
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-04 10:00:13 -0400

    Add application

---
+++ app/src/main/java/com/example/budgetingapplication/BudgetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.budgetingapplication
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class BudgetApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { AppDatabase.getInstance(this, applicationScope) }
+    val repository by lazy { BudgetRepository(database.budgetDao()) }
+}
