7d2f179fd7ca788187bd450e9e72dc7e0df91539
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-04 09:46:58 -0400

    Add database.

---
+++ app/src/main/java/com/example/budgetingapplication/AppDatabase.kt
@@ -0,0 +1,54 @@
+package com.example.budgetingapplication
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Database(entities = [Budget::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun budgetDao(): BudgetDao
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context, scope: CoroutineScope): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: buildDatabase(context, scope).also { instance = it }
+            }
+        }
+
+        private fun buildDatabase(context: Context, scope: CoroutineScope): AppDatabase {
+            return Room.databaseBuilder(context, AppDatabase::class.java, "budget_database")
+                .addCallback(
+                    object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            instance?.let { database ->
+                                scope.launch(Dispatchers.IO) {
+                                    populateDatabase(database.budgetDao())
+                                }
+                            }
+                        }
+                    }
+                )
+                .build()
+        }
+
+        suspend fun populateDatabase(budgetDao: BudgetDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            budgetDao.deleteAll()
+
+            val budget = listOf(Budget("Transportation"), Budget("Home food"), Budget("Junk food"), Budget("Entertainment"), Budget("Housing"), Budget("Gifts"))
+            budgetDao.insertAll(budget)
+        }
+    }
+}
