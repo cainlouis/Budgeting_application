2cd7b385235770f2dfa707a58f99b1f5bf5a97bf
Nael Louis <nael.louis1002@gmail.com>
2021-11-05 22:30:58 -0400

    Add comments and get rid of useless code

--- app/src/main/java/com/example/budgetingapplication/AddSpendingActivity.kt
+++ app/src/main/java/com/example/budgetingapplication/AddSpendingActivity.kt
@@ -9,8 +9,13 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.budgetingapplication.model.Budget
 import com.example.budgetingapplication.viewmodel.BudgetViewModel
 
+/**
+ * @author Zacharie Makeen and Nael Louis
+ * This class allows the user to add money to each category
+ */
 class AddSpendingActivity : MenuActivity() {
 
+    //Initialize the variables that will later be used
     private lateinit var budgetViewModel: BudgetViewModel
     lateinit var spinner: Spinner
 
@@ -18,6 +23,7 @@ class AddSpendingActivity : MenuActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_spending)
 
+        //get the view model that will be used to interact with db
         budgetViewModel = ViewModelProvider(this).get(BudgetViewModel::class.java)
 
         spinner = findViewById(R.id.categorySpinner)
@@ -36,6 +42,7 @@ class AddSpendingActivity : MenuActivity() {
         lateinit var  category: String
         lateinit var  categoryIndex: String
 
+        //Set the values in the spinner from the array in string.xml
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 category = resources.getStringArray(R.array.categories_array)[p2].toString()
@@ -46,29 +53,33 @@ class AddSpendingActivity : MenuActivity() {
             }
         }
 
-
+        //Initiate budget1 as the readAll which returns a liveData list
         val budgets1 = budgetViewModel.readAll
         budgets1.observe(this, Observer { budgets ->
-            //
+            //For testing
         })
 
 
         val updateButton = findViewById<Button>(R.id.updateButton)
-
+        //on click
         updateButton.setOnClickListener {
+            //get the user input
             val moneySpent = findViewById<EditText>(R.id.moneySpentInput).text.toString().toDouble()
+            //format it before setting into db
             val moneySpentCleaned = String.format(resources.getString(R.string.format), moneySpent).toDouble()
 
+            //Get the list of categories and their values from db
             val budgets = budgetViewModel.readAll
             var currentMoneySpent: Double = 0.0
+            //set the value from the db
             budgets.observe(this, Observer { budgets2 ->
                 currentMoneySpent = budgets2[categoryIndex.toInt()].spending
             })
-
+            //add to existing values and insert into db
             val budget = Budget(category, moneySpentCleaned + currentMoneySpent)
             budgetViewModel.update(budget)
 
-
+            //notify the user that it was successfully added
            Toast.makeText(this, resources.getString(R.string.onAddSpending), Toast.LENGTH_LONG).show()
         }
     }
