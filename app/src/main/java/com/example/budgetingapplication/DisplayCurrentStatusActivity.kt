0e0a8e822590191634cd2cac03cf565a4c5aa701
Zacharie Makeen <zach.makeen@gmail.com>
2021-11-21 13:06:13 -0500

    Adapter layout

--- app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusActivity.kt
+++ app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusActivity.kt
@@ -2,14 +2,34 @@ package com.example.budgetingapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.budgetingapplication.viewmodel.BudgetViewModel
 
 /**
  * @author Kirill Parhomenco
  * this activity display the value of each category fetched from the database
  */
 class DisplayCurrentStatusActivity : MenuActivity() {
+
+    private lateinit var budgetViewModel: BudgetViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_current_status)
+
+        //get the view model that will be used to interact with db
+        budgetViewModel = ViewModelProvider(this).get(BudgetViewModel::class.java)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = DisplayCurrentStatusAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        budgetViewModel.readAll.observe(this, Observer { budgets ->
+            adapter.setData(budgets)
+        })
     }
 }
--- app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusAdapter.kt
+++ app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusAdapter.kt
@@ -3,6 +3,8 @@ package com.example.budgetingapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.budgetingapplication.model.Budget
 
@@ -11,14 +13,18 @@ class DisplayCurrentStatusAdapter: RecyclerView.Adapter<DisplayCurrentStatusAdap
     private var budgetList = emptyList<Budget>()
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-//        fun bind(text: String?) {
-//            wordItemView.text = text
-//        }
+        private val category: TextView = itemView.findViewById(R.id.category)
+        private val spending: TextView = itemView.findViewById(R.id.spending)
+
+        fun bind(categoryText: String?, spendingText: String?) {
+            category.text = categoryText
+            spending.text = spendingText
+        }
 
         companion object {
             fun create(parent: ViewGroup): MyViewHolder {
                 val view: View = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.activity_display_current_status, parent, false)
+                    .inflate(R.layout.recyclerview_item, parent, false)
                 return MyViewHolder(view)
             }
         }
@@ -30,10 +36,15 @@ class DisplayCurrentStatusAdapter: RecyclerView.Adapter<DisplayCurrentStatusAdap
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentItem = budgetList[position]
-        holder.itemView.
+        holder.bind(currentItem.category, currentItem.spending.toString())
     }
 
     override fun getItemCount(): Int {
         return budgetList.size
+    }
+
+    fun setData(budget: List<Budget>) {
+        this.budgetList = budget
+        notifyDataSetChanged()
     }
 }
