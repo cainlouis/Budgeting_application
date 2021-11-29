4ef011394b98898440e4636b4f2c1df2f4ee2c43
Kirill Parhomenco <6976451-greenmayo@users.noreply.gitlab.com>
2021-11-28 21:39:19 -0500

    Made status work properly

--- app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusActivity.kt
+++ app/src/main/java/com/example/budgetingapplication/DisplayCurrentStatusActivity.kt
@@ -2,14 +2,43 @@ package com.example.budgetingapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.budgetingapplication.viewmodel.BudgetViewModel
 
 /**
  * @author Kirill Parhomenco
  * this activity display the value of each category fetched from the database
  */
 class DisplayCurrentStatusActivity : MenuActivity() {
+
+    private lateinit var budgetViewModel: BudgetViewModel
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        budgetViewModel = ViewModelProvider(this).get(BudgetViewModel::class.java)
         setContentView(R.layout.activity_display_current_status)
+
+        // setup text views
+        val transportation = findViewById<TextView>(R.id.transportation)
+        val homeFood = findViewById<TextView>(R.id.homeFood)
+        val junkFood = findViewById<TextView>(R.id.junkFood)
+        val entertainment = findViewById<TextView>(R.id.entertainment)
+        val housing = findViewById<TextView>(R.id.housing)
+        val gifts = findViewById<TextView>(R.id.gifts)
+
+        val budgets = budgetViewModel.readAll
+
+        budgets.observe(this, {data ->
+            transportation.text = data[0].spending.toString()
+            homeFood.text = data[1].spending.toString()
+            junkFood.text = data[2].spending.toString()
+            entertainment.text = data[3].spending.toString()
+            housing.text = data[4].spending.toString()
+            gifts.text = data[5].spending.toString()
+        })
     }
 }
