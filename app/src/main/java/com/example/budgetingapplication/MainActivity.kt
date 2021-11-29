5543c6664f61954c7639b8fb76b988175ff7d504
Hieu Nghia Phan <hieunghiabeo@gmail.com>
2021-11-28 21:39:54 -0500

    Readd pie chart with different implementation

--- app/src/main/java/com/example/budgetingapplication/MainActivity.kt
+++ app/src/main/java/com/example/budgetingapplication/MainActivity.kt
@@ -1,53 +1,31 @@
 package com.example.budgetingapplication
 
-import android.graphics.Color
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.androidplot.pie.PieChart
-import com.androidplot.pie.Segment
-import com.androidplot.pie.SegmentFormatter
-import com.example.budgetingapplication.model.Budget
-import com.example.budgetingapplication.viewmodel.BudgetViewModel
+import android.view.View
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.charts.Pie
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
 
 class MainActivity : MenuActivity() {
-
-    private lateinit var budgetViewModel: BudgetViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val arrayList = ArrayList<Int>()
+        val pie = AnyChart.pie()
 
-        arrayList.add(20)
-        arrayList.add(20)
-        arrayList.add(5)
-        arrayList.add(15)
-        arrayList.add(30)
-        arrayList.add(10)
+        val data: MutableList<DataEntry> = ArrayList()
+        data.add(ValueDataEntry("Transportation", 10000 ))
+        data.add(ValueDataEntry("Home food", 20000 ))
+        data.add(ValueDataEntry("Junk food", 5000 ))
+        data.add(ValueDataEntry("Entertainment", 20000 ))
+        data.add(ValueDataEntry("Housing", 40000 ))
+        data.add(ValueDataEntry("Gifts", 5000 ))
 
-        val transportation = Segment("Transportation", arrayList.get(0))
-        val homeFood = Segment("Home Food", arrayList.get(1))
-        val junkFood = Segment("Junk Food", arrayList.get(2))
-        val entertainment = Segment("Entertainment", arrayList.get(3))
-        val housing = Segment("Housing", arrayList.get(4))
-        val gifts = Segment("Gift", arrayList.get(5))
+        pie.data(data)
 
-        val transportationSF = SegmentFormatter(Color.BLACK)
-        val homeFoodSF = SegmentFormatter(Color.RED)
-        val junkFoodSF = SegmentFormatter(Color.DKGRAY)
-        val entertainmentSF = SegmentFormatter(Color.MAGENTA)
-        val housingSF = SegmentFormatter(Color.YELLOW)
-        val giftsSF = SegmentFormatter(Color.BLUE)
-
-
-        val pieChart = findViewById<PieChart>(R.id.pieChart)
-
-        pieChart.addSegment(transportation, transportationSF)
-        pieChart.addSegment(homeFood, homeFoodSF)
-        pieChart.addSegment(junkFood, junkFoodSF)
-        pieChart.addSegment(entertainment, entertainmentSF)
-        pieChart.addSegment(housing, housingSF)
-        pieChart.addSegment(gifts, giftsSF)
+        val anyChartView = findViewById<View>(R.id.pieChart) as AnyChartView
+        anyChartView.setChart(pie)
     }
 }
